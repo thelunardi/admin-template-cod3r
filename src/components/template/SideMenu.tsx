49bdc93151c238d4fd3ad3812cd8323fd4f7af7a
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2021-10-07 19:56:35 -0300

    :sparkles: add menu item, notifications, settings, logo

--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -1,7 +1,29 @@
+import MenuItem from './MenuItem'
+import {HomeIcon, LogoutIcon, NotificationIcon, SettingsIcon} from '../icons'
+import Logo from './Logo'
+
 export default function SideMenu() {
     return (
-        <aside>
-            <h1>Menu</h1>
+        <aside className={`flex flex-col`}>
+            <div className={`
+                flex flex-col items-center justify-center
+                h-20 w-20
+                bg-gradient-to-r from-indigo-500 to-purple-800
+            `}>
+                <Logo />
+            </div>
+            <ul className={`flex-grow`}>
+                <MenuItem url='/' text='Home' icon={HomeIcon} />
+                <MenuItem url='/settings' text='Settings' icon={SettingsIcon} />
+                <MenuItem url='/notifications' text='Notifications' icon={NotificationIcon} />
+            </ul>
+            <ul>
+                <MenuItem
+                    className={`text-red-600 hover:bg-red-400 hover:text-white`}
+                    onClick={() => console.log('asdas')}
+                    text='Sair'
+                    icon={LogoutIcon} />
+            </ul>
         </aside>
     )
 }
---
+++ src/pages/settings.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/template/Layout'
+
+export default function Settings() {
+    return (
+        <Layout title='Settings' subtitle='Gerencia de settings'>
+            <h3>Conteúdo</h3>
+        </Layout>
+    )
+}
